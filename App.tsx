7dd043d3b31fefecdc89ee3270f4f3ff3ae732f1
Bruno Santos <brunojsfla@gmail.com>
2022-03-27 11:06:45 -0300

    feat: implementação da tela de detalhes do carro

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 'react-native-gesture-handler';
 import {
   Archivo_400Regular,
   Archivo_500Medium,
@@ -12,7 +13,7 @@ import {
 import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components/native";
 
-import { Home } from "./src/screens/Home";
+import { CarDetails } from "./src/screens/CarDetails";
 import theme from "./src/styles/theme";
 
 export default function App() {
@@ -29,7 +30,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+import { Accessory } from "../../components/Accessory";
+import { Button } from "../../components/Button";
+
+import SpeedSvg from "../../assets/speed.svg";
+import AccelerationSvg from "../../assets/acceleration.svg";
+import ForceSvg from "../../assets/force.svg";
+import GasolineSvg from "../../assets/gasoline.svg";
+import ExchangeSvg from "../../assets/exchange.svg";
+import PeopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Acessories,
+  Footer,
+} from "./styles";
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "http://fordsuperauto.com.br/img/carros/fiesta-hatch/NewFiestaHatch34F-PrataDublin.jpg",
+          ]}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Ford</Brand>
+            <Name>New Fiesta</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 100,00</Price>
+          </Rent>
+        </Details>
+
+        <Acessories>
+          <Accessory name="380 km/h" icon={SpeedSvg} />
+          <Accessory name="3.2 s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Acessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Escolher período do aluguel" />
+      </Footer>
+    </Container>
+  );
+}
